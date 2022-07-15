35f1199ab600d8bed0ea087c58eed56d660a4d3f
Nilton Cosme Felix da Costa Junior <66050129+ncjr1@users.noreply.github.com>
2022-07-15 11:28:48 -0300

    Updates to index.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Card from '../components/Card'
+import { useState } from "react"
 
-function Home({ pokemons }) {
+function Home({ pokemons, ...props }) {
+  const [ pokemonList, setPokemon ] = useState(pokemons);
+  const [ filter, setFilter ] = useState('');
   return (
-  <>
+  <div>
+    <div id="search">
+      <input key="searchBar" type="text" placeholder="Filter Pokemon by Name" value={filter} onChange={(e) => { setFilter(e.target.value) } } style={{ width: 340 + 'px'}}></input>
+    </div>
     <div id="home">
-      {pokemons.map((pokemon) => { return <Card Title={pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)} Url={pokemon.url} Number={pokemon.url.slice(33, 38).replaceAll("/", "")}></Card>})}
+      {pokemonList.filter(pokemon => pokemon.name.includes(filter) || filter === "").map((pokemon) => { return <Card Title={pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)} Url={pokemon.url} Number={pokemon.url.slice(33, 38).replaceAll("/", "")}></Card>})}
     </div>
     <style jsx>{`
       #home {
@@ -15,13 +21,21 @@ function Home({ pokemons }) {
         justify-content: space-around;
         flex-flow: row wrap;
       }
+      #search{
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        margin-bottom: 3%;
+        margin-top: 3%;
+      }
     `}</style>
-  </>
+  </div>
   )
 }
 
 export async function getStaticProps (context) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10`)
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=300`)
   const data = await res.json()
   if (!data) {
     return {
